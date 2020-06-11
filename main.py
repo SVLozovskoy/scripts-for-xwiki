791d0dd2b09510c82dcd3a70170b4d3e66b613af
svlozovskoy <sergey_lozovskoy@outlook.com>
2020-06-11 21:34:42 +0300

    auto scripts for xwiki

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+
+def pkg_install():
+    os.system('apt-get install docker docker.io docker-compose')
+    os.system('apt-get install openssl')
+
+def ssl_generate():
+    os.system('mkdir cert')
+    os.system('sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./cert/apache-selfsigned.key -out cert/apache-selfsigned.crt -subj "/C=US/ST=Ohio/L=Columbus/O=Widgets Inc/OU=Some Unit"')
+
+
+def change_right():
+    os.system('chmod -R 777 cert')
+    print('done')
+
+def run_docker():
+    os.system('docker-compose up')
+
+pkg_install()
+ssl_generate()
+change_right()
+run_docker()
